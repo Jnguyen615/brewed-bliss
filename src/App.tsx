47ebcc9263a177ab5239da73e25230eacb626eea
Jen Nguyen <jenniferannnguyen@live.com>
2024-04-18 09:58:40 -0600

    add api call for all teas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import Header from './components/Header/Header';
 import Search from './components/Search/Search';
 import teaBackground from '../src/images/tea-background.jpg'
+import { getTeas } from '../src/api-call'
+import { useEffect } from 'react'
+
 
 function App() {
+
+  useEffect(()=> {
+    getTeas()
+  }, [])
+
   return (
     <div className='bg-cover bg-no-repeat bg-center h-screen' style={{backgroundImage: `url(${teaBackground})`}}>
     <div className="bg-black bg-opacity-40 h-full">
---
+++ src/api-call.tsx
@@ -0,0 +1,20 @@
+export const getTeas = async () => {
+
+  try {
+    const response = await fetch(
+      "https://boonakitea.cyclic.app/api/all"
+    );
+
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log(data)
+    return data;
+  } catch (error) {
+   console.log('Network error: Please check your internet connection');
+    
+    throw error;
+  }
+};
